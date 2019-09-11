1f4995de3ee00c074085db5c91c43d756de633d5
Johan <johanmunneke94@gmail.com>
2019-09-11 09:05:13 +0200

    Initialize project and widget

---
+++ CustomWidgets/dynamicEnumPicker/README.md
@@ -0,0 +1,17 @@
+## DynamicEnumPicker
+[Choose which enumeration values to show as dropdown or radiobutton list and change captions as needed]
+
+## Features
+[feature highlights]
+
+## Usage
+[step by step instructions]
+
+## Demo project
+[link to sandbox]
+
+## Issues, suggestions and feature requests
+[link to GitHub issues]
+
+## Development and contribution
+[specify contribute]
---
+++ CustomWidgets/dynamicEnumPicker/typings/DynamicEnumPickerProps.d.ts
@@ -0,0 +1,25 @@
+/**
+ * This file was generated from DynamicEnumPicker.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix Widgets Team
+ */
+import { CSSProperties } from "react";
+
+interface CommonProps {
+    id: string;
+    class: string;
+    style?: CSSProperties;
+    tabIndex: number;
+}
+
+export interface DynamicEnumPickerContainerProps extends CommonProps {
+    sampleText?: string;
+}
+
+export interface DynamicEnumPickerPreviewProps extends CommonProps {
+    sampleText?: string;
+}
+
+export interface VisibilityMap {
+    sampleText: boolean;
+}
